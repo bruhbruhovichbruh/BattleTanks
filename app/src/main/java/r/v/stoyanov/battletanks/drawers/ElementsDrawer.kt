4c9aa263cb4b49eccec0b7c0289b00dd270d4128
bruhbruhovichbruh <onlytomorrow@proton.me>
2025-05-24 10:12:19 +0300

    v6

--- app/src/main/java/r/v/stoyanov/battletanks/drawers/ElementsDrawer.kt
+++ app/src/main/java/r/v/stoyanov/battletanks/drawers/ElementsDrawer.kt
@@ -23,6 +23,36 @@ class ElementsDrawer(val container: FrameLayout) {
         val leftMargin = x.toInt() - (x.toInt() % CELL_SIZE)
         val coordinate = Coordinate(topMargin, leftMargin)
         drawView(coordinate)
+        if (currentMaterial == Material.EMPTY) {
+            eraseView(coordinate)
+        } else {
+            drawOrReplaceView(coordinate);
+        }
+    }
+
+    private fun drawOrReplaceView(coordinate: Coordinate) {
+        val viewOnCoordinate = getElementByCoordinates(coordinate)
+        if (viewOnCoordinate == null) {
+            drawView(coordinate)
+            return
+        }
+        if (viewOnCoordinate.material != currentMaterial) {
+            replaceView(coordinate)
+        }
+    }
+
+    private fun replaceView(coordinate: Coordinate) {
+        eraseView(coordinate);
+        drawView(coordinate);
+    }
+
+    private fun eraseView(coordinate: Coordinate) {
+        val elementOnCoordinate = getElementByCoordinates(coordinate)
+        if (elementOnCoordinate != null) {
+            val erasingView = container.findViewById<View>(elementOnCoordinate.viewId)
+            container.removeView(erasingView)
+            elementsOnContainer.remove(elementOnCoordinate)
+        }
     }
 
     fun drawView(coordinate: Coordinate) {
