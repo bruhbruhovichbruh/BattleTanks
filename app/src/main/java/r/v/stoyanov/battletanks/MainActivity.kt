79608f6df74796b64c2b68a85349a1d1dadf7ff3
bruhbruhovichbruh <onlytomorrow@proton.me>
2025-05-24 17:42:38 +0300

    v15

--- app/src/main/java/r/v/stoyanov/battletanks/MainActivity.kt
+++ app/src/main/java/r/v/stoyanov/battletanks/MainActivity.kt
@@ -12,6 +12,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.view.View.GONE
 import r.v.stoyanov.battletanks.enums.Direction.UP
 import r.v.stoyanov.battletanks.enums.Direction.DOWN
 import r.v.stoyanov.battletanks.enums.Direction.LEFT
@@ -67,18 +68,28 @@ class MainActivity : AppCompatActivity() {
             return@setOnTouchListener true
         }
         elementsDrawer.drawElementsList(levelStorage.loadLevel())
+        hideSettings()
     }
 
     private fun switchEditMode() {
-        if  (editMode) {
-            gridDrawer.removeGrid()
-            binding.materialsContainer.visibility = INVISIBLE
-        } else {
-            gridDrawer.drawGrid()
-            binding.materialsContainer.visibility = VISIBLE
-        }
-
         editMode = !editMode
+        if  (editMode) {
+            showSettings()
+        } else {
+            hideSettings()
+        }
+    }
+
+    private fun showSettings() {
+        gridDrawer.drawGrid()
+        binding.materialsContainer.visibility = VISIBLE
+        elementsDrawer.changeElementsVisibility(true)
+    }
+
+    private fun hideSettings() {
+        gridDrawer.removeGrid()
+        binding.materialsContainer.visibility = GONE
+        elementsDrawer.changeElementsVisibility(false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
